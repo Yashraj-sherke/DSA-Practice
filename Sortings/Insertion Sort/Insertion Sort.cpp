fae0831f818418d2cc23305821500ce5ce9f5d42
Yashraj Sherke <145894334+Yashraj-sherke@users.noreply.github.com>
2025-08-06 12:28:21 +0530

    Create Insertion Sort.cpp

---
+++ Sortings/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,15 @@
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements of arr[0..i-1] greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key; // Insert key
+    }
+}
