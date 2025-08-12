185dad28e64eff7eeeed9b8b48ca10bbb15f08ec
Yashraj Sherke <145894334+Yashraj-sherke@users.noreply.github.com>
2025-08-13 00:36:58 +0530

    Create Search Insert Position.cpp

---
+++ Binary Search/Search Insert Position/Search Insert Position.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int start =0;
+        int end = nums.size()-1;
+        int ans = nums.size();
+
+        while(start<=end)
+        {
+            int mid= start+(end-start)/2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                // search in left side
+                ans= mid;
+                end = mid-1;
+            }
+            else
+            {
+                start= mid+1;
+            }
+        }
+
+        return  ans;
+        
+    }
+};
