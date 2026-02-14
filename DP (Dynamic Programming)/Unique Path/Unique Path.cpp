82a3442c602f992644c5aa3d0d4a017a08a213c5
Yashraj Sherke <145894334+Yashraj-sherke@users.noreply.github.com>
2026-02-14 12:33:09 +0530

    Update Unique Path.cpp

--- DP (Dynamic Programming)/Unique Path/Unique Path.cpp
+++ DP (Dynamic Programming)/Unique Path/Unique Path.cpp
@@ -27,3 +27,37 @@ public:
         
     }
 };
+// Bottom Up approch 
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+    vector<vector<int>>t(m,vector<int>(n));
+
+    t[0][0]=1;
+
+    //0th row
+
+    for(int col=1 ;col<n;col++)
+    {
+        t[0][col]=1;
+    }
+
+    // 0th col
+    for(int row=1; row<m;row++){
+        t[row][0]=1;
+    }
+
+    for(int i=1;i<m;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            t[i][j]=t[i-1][j]+t[i][j-1];
+        }
+
+        
+    }
+    return t[m-1][n-1];
+    }
+};
