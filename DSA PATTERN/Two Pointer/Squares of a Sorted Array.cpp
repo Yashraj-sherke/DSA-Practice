b1ddc67046cddde69814ab51195248f372c9dc75
Yashraj Sherke <145894334+Yashraj-sherke@users.noreply.github.com>
2026-05-08 12:10:28 +0530

    Add sortedSquares function using two-pointer technique
    Implement two-pointer approach to return sorted squares of the input array.

---
+++ DSA PATTERN/Two Pointer/Squares of a Sorted Array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        // int n=nums.size();
+
+        // vector<int>ans;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     ans.push_back(nums[i]*nums[i]);
+        // }
+
+        // sort(ans.begin(),ans.end());
+
+        // return ans;
+
+
+
+        int n = nums.size();
+
+        vector<int> ans(n);
+
+        int left = 0;
+        int right = n - 1;
+
+        int index = n - 1;
+
+        while(left <= right)
+        {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if(leftSquare > rightSquare)
+            {
+                ans[index] = leftSquare;
+                left++;
+            }
+            else
+            {
+                ans[index] = rightSquare;
+                right--;
+            }
+
+            index--;
+        }
+
+        return ans;
+ 
+        
+    }
+};
